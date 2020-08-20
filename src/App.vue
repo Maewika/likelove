ca98de3379357b6b5d8ff3daf043d0a0743812cd
Maewika <68525359+Maewika@users.noreply.github.com>
2020-08-20 20:58:04 +0700

    likelove

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" width="45%" src="./assets/pngfind.com-smiley-png-40051.png">
+    <HelloWorld msg="ชอบก็กดไลค์ ใช่ก็กดเลิฟนะคะ" />
+    <div class="row">
+      <div class="col-6">
+        <img alt="Vue logo" width="50%" src="./assets/1.jpg" />
+      <h1>{{total1}}</h1>
+      </div>
+      <div class="col-6">
+        <img alt="Vue logo" width="50%" src="./assets/2.jpg" />
+        <h1>{{total2}}</h1>
+      </div>
+      
+      <div class="col-6">
+        <CSButton @cc="sum" />
+      </div>
+      <div class="col-6">
+        <CSButton @cc="sum1" />
+      </div>
+      
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import CSButton from "./components/CsButton.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    CSButton,
+  },
+
+  data: function () {
+    return {
+      total1: 0,
+      total2: 0,
+    };
+  },
+
+  methods: {
+    sum(counterValue) {
+      this.total1 = counterValue;
+    },
+    sum1(counterValue) {
+      this.total2 = counterValue;
+    },
+  },
+};
 </script>
 
 <style>
+body {
+  background-image: url("./assets/4.jpg") ;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
